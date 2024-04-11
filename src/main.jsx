b5ddafaad30a7ff7119801f7999c37bcd5abcc80
hvdoanh <hvdoanh1107@gmail.com>
2024-04-11 15:21:19 +0700

    basic_layout

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
-import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-f
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssVarsProvider theme={theme}>
--- src/theme.js
+++ src/theme.js
@@ -5,20 +5,26 @@ import { teal, deepOrange, cyan, orange } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = extendTheme({
+  trelloCustom: {
+    appBarHeight : "90px",
+    boardBarHeight : "125px"
+  },
+
+
   colorSchemes: {
     light: {
       palette: {
         primary: teal,
         secondary: deepOrange,
-      }
+      },
     },
     dark: {
       palette: {
         primary: cyan,
-        secondary: orange
-      }
-    }
-  }
+        secondary: orange,
+      },
+    },
+  },
   // ...other properties
 });
 
